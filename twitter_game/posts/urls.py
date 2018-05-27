83968b72cbccdb1cdb5e000ef69c99b55c8f6e0e
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-05-27 13:35:50 -0700

    the like button is working.

--- twitter_game/posts/urls.py
+++ twitter_game/posts/urls.py
@@ -1,8 +1,20 @@
 """URL configuration for posts."""
 from django.conf.urls import re_path
 from posts.views import PostCreateView
+from . import views
+from django.urls import path
+
 
 urlpatterns = [
     re_path(r'^create/$', PostCreateView.as_view(), name='create'),
-    re_path(r'^like/$', 'posts.views.like', name='like'),
+    path('like/', views.like, name='like'),
 ]
+
+
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     path('', views.like, name='like'),
+# ]
--- twitter_game/posts/views.py
+++ twitter_game/posts/views.py
@@ -1,9 +1,15 @@
 """App views."""
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from posts.models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from posts.models import Like
+
+from django.shortcuts import get_object_or_404
+import json
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -21,3 +27,27 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         """Assign the user to the foreign key in the model."""
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+# @login_required
+@require_http_methods(["POST"])
+def like(request):
+    """Handle a user like."""
+    user = request.user
+    post_id = request.POST.get('post_id', None)
+    post = get_object_or_404(Post, id=post_id)
+
+    the_like, created = Like.objects.get_or_create(
+        user=user,
+        post=post)
+
+    if created:
+        message = 'You liked this'
+    else:
+        the_like.delete()
+        message = 'You unliked this'
+
+    likes_count = post.like.count()
+
+    ctx = {'likes_count': likes_count, 'message': message}
+    return HttpResponse(json.dumps(ctx), content_type='application/json')
